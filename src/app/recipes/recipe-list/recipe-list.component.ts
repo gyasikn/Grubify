c249c242a5e0c1c9322708e19e6ad1a20b2d9112
Gyasik Nunez <gyasiknunez@gmail.com>
2018-03-20 16:38:44 -0400

    navigation routes working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("Awesome Recipe WOW", "Just Amazing...", "http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg"),
     new Recipe("Much Recipe wow", "omg delisshhh...", "http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg"),
-    new Recipe("Wow Great Food Even Better Wow", "finger licking good...", "http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg")
+    new Recipe("Food Even Better Wow", "finger licking good...", "http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg")
   ];
 
   constructor() {}
