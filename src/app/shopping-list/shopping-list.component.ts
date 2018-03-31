94f79a4061a50cf3374ff484bd59cad860b89120
Gyasik Nunez <gyasiknunez@gmail.com>
2018-03-30 22:49:14 -0400

    added buttons for recipes and shopping list views

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -5,36 +5,33 @@ import { Ingredient } from  '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
-
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private subscription: Subscription
+  private subscription: Subscription;
 
-
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => {
-          this.ingredients = ingredients;
-        }
-      )
+    this.subscription = this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
   onEditItem(index: number) {
     this.slService.startedEditing.next(index);
   }
 
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
     // to prevent any memory leaks
   }
 
-  
 }
